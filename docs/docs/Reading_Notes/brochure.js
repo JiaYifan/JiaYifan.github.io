1f964ac5907dd6c452912309a0f1b4ba410311d3
JiaYifan <347768282@qq.com>
2019-02-21 12:39:38 +0800

    2.21

--- docs/docs/Reading_Notes/brochure.js
+++ docs/docs/Reading_Notes/brochure.js
@@ -14,18 +14,180 @@ try{
     console.log(e)
 }
 
-function deepClone(obj){
-    function isObj(obj){
-        return (typeof(obj)==='object'||typeof(obj)==='function')&&obj!==null
-    }
+// 手写深拷贝
+function deepClone(obj) {
+  function isObject(o) {
+    return (typeof o === 'object' || typeof o === 'function') && o !== null
+  }
 
-    if (!isObj(obj)) {
-        throw new Error('非对象')
-    }
+  if (!isObject(obj)) {
+    throw new Error('非对象')
+  }
 
-    let copy = Array.isArray(obj) ? [...obj]:{...obj}
-    Reflect.ownKeys(copy).forEach(key=>{
-        copy[key] = isObj(obj[key])?deepClone(obj[key]):obj[key]
-    })
-    return copy
+  let isArray = Array.isArray(obj)
+  let newObj = isArray ? [...obj] : { ...obj }
+  Reflect.ownKeys(newObj).forEach(key => {
+    newObj[key] = isObject(obj[key]) ? deepClone(obj[key]) : obj[key]
+  })
+
+  return newObj
 }
+
+// 手写Promise
+const PENDING = 'pending'
+const RESOLVED = 'resolved'
+const REJECTED = 'rejected'
+
+function MyPromise(fn) {
+  const that = this
+  that.state = PENDING
+  that.value = null
+  that.resolvedCallbacks = []
+  that.rejectedCallbacks = []
+  function resolve(value) {
+    if (that.state === PENDING) {
+      that.state = RESOLVED
+      that.value = value
+      that.resolvedCallbacks.map(cb => cb(that.value))
+    }
+  }
+  
+  function reject(value) {
+    if (that.state === PENDING) {
+      that.state = REJECTED
+      that.value = value
+      that.rejectedCallbacks.map(cb => cb(that.value))
+    }
+  }
+  try {
+    fn(resolve, reject)
+  } catch (e) {
+    reject(e)
+  }
+}
+MyPromise.prototype.then = function(onFulfilled, onRejected) {
+  const that = this
+  onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : v => v
+  onRejected =
+    typeof onRejected === 'function'
+      ? onRejected
+      : r => {
+          throw r
+        }
+  if (that.state === PENDING) {
+    that.resolvedCallbacks.push(onFulfilled)
+    that.rejectedCallbacks.push(onRejected)
+  }
+  if (that.state === RESOLVED) {
+    onFulfilled(that.value)
+  }
+  if (that.state === REJECTED) {
+    onRejected(that.value)
+  }
+}
+
+// 手写call
+Function.prototype.myCall = function(context) {
+  if (typeof this !== 'function') {
+    throw new TypeError('Error')
+  }
+  context = context || window
+  context.fn = this
+  const args = [...arguments].slice(1)
+  const result = context.fn(...args)
+  delete context.fn
+  return result
+}
+// 若要较真下面这种情况的话
+function sayFn () {
+  console.log(this.fn)
+}
+sayFn.myCall({
+  fn: 'Im fn'
+})
+
+Function.prototype.myCall = function(context) {
+  if (typeof this !== 'function') {
+    throw new TypeError('Error')
+  }
+  context = context || window
+  let mySymbol = Symbol();
+  context[mySymbol] = this
+  const args = [...arguments].slice(1)
+  const result = context[mySymbol](...args)
+  delete context[mySymbol]
+  return result
+}
+
+// 手写apply
+Function.prototype.myApply = function(context) {
+  if (typeof this !== 'function') {
+    throw new TypeError('Error')
+  }
+  context = context || window
+  context.fn = this
+  let result
+  // 处理参数和 call 有区别
+  if (arguments[1]) {
+    result = context.fn(...arguments[1])
+  } else {
+    result = context.fn()
+  }
+  delete context.fn
+  return result
+}
+
+// 手写bind
+Function.prototype.myBind = function (context) {
+  if (typeof this !== 'function') {
+    throw new TypeError('Error')
+  }
+  const _this = this
+  const args = [...arguments].slice(1)
+  // 返回一个函数
+  return function F() {
+    // 因为返回了一个函数，我们可以 new F()，所以需要判断
+    if (this instanceof F) {
+      return new _this(...args, ...arguments)
+    }
+    return _this.apply(context, args.concat(...arguments))
+  }
+}
+
+
+Function.prototype.myBind = function (context) {
+  if (typeof this !== 'function') {
+    throw new TypeError('Error')
+  }
+  const _this = this
+  const args = [...arguments].slice(1)
+  console.log(...arguments)
+  console.log(...args)
+  // 返回一个函数
+  return function F() {
+    // 因为返回了一个函数，我们可以 new F()，所以需要判断
+    if (this instanceof F) {
+      return new _this(...args, ...arguments)
+    }
+    return _this.apply(context, args.concat(...arguments))
+  }
+}
+
+this.x = 9;    // 在浏览器中，this指向全局的 "window" 对象
+var module = {
+  x: 81,
+  getX: function() { 
+    console.log(arguments);
+    return this.x; 
+  }
+};
+module.getX(); // 81
+
+var retrieveX = module.getX;
+retrieveX();   
+// 返回9 - 因为函数是在全局作用域中调用的
+
+// 创建一个新函数，把 'this' 绑定到 module 对象
+// 新手可能会将全局变量 x 与 module 的属性 x 混淆
+var boundGetX = retrieveX.bind(module);
+boundGetX(); // 81
